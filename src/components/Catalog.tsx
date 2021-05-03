78093c229ff0d09fa9d4d0151fd0368fe3d06b26
DevMiguell <buguel2000@gmail.com>
2021-05-03 08:47:20 -0300

    criabdo fake api e configurando o dispach

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 
- const Catalog: React.FC = () => {
-  const catalog = useSelector(state => state)
+import { IProduct } from '../store/modules/cart/types'
+import api from '../services/api'
+import { addProductToCart } from '../store/modules/cart/action'
+
+const Catalog: React.FC = () => {
+  const dispach = useDispatch()
+  const [catalog, setCatalog] = useState<IProduct[]>([])
+
+  useEffect(() => {
+    api.get('products').then(response => {
+      setCatalog(response.data)
+    })
+  }, [])
+
+  const handleAddProductToCart = useCallback((product: IProduct) => {
+    dispach(addProductToCart(product))
+  }, [dispach])
 
   return (
-    <h1>Catalog</h1>
+    <main>
+      <h1>Catalog</h1>
+      {catalog.map(product => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {'  -  '}
+          <span>{product.price}</span> {'  '}
+
+          <button type="button" onClick={() => handleAddProductToCart(product)}>Comprar</button>
+        </article>
+      ))}
+    </main>
   )
 }
 
