6a0dfde640df80b44bd54bc4ef297d37d7991206
DevMiguell <buguel2000@gmail.com>
2021-05-03 09:59:39 -0300

    Organizando codigo

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,12 +1,31 @@
 import { Reducer } from "redux";
 import { ICartState } from "./types";
+import produce from 'immer'
 
 const INICIAL_STATE: ICartState = {
   items: []
 }
 
-const cart: Reducer<ICartState> = () => {
-  return INICIAL_STATE;
+const cart: Reducer<ICartState> = (state = INICIAL_STATE, action) => {
+  return produce(state, draft => {
+    switch (action.type) {
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload
+
+        draft.items.push({
+          product,
+          quantity: 1,
+        })
+
+        break
+      }
+      default: {
+        return draft
+      }
+    }
+  })
 }
+
+
 
 export default cart;
